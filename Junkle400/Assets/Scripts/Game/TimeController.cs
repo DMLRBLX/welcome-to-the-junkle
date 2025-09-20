f41ffe37f472ced506851a5353444d8c1536b15a
Jerry Denning (Error 102) <109883821+DMLRBLX@users.noreply.github.com>
2025-09-20 02:29:35 -0600

    Countdown and Timer done

---
+++ Junkle400/Assets/Scripts/Game/TimeController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using UnityEngine;
+using TMPro;
+using Unity.VisualScripting;
+
+public class TimeController : MonoBehaviour
+{
+    [Header("Match")]
+    [SerializeField] TextMeshProUGUI matchTimeText;
+    [SerializeField] string matchTimeMessage;
+    [SerializeField] string matchTimeMinutesReplacement;
+    [SerializeField] string matchTimeSecondsReplacement;
+    [SerializeField] int matchTimeMinutes = 5;
+    [SerializeField] int matchTimeSeconds = 0;
+
+    [Header("Countdown")]
+    [SerializeField] TextMeshProUGUI countdownText;
+    [SerializeField] string countdownMessage;
+    [SerializeField] string countdownNumberReplacement;
+    [SerializeField] string endMatchMessage;
+    [SerializeField] int countdown;
+
+    void Start()
+    {
+        StartCoroutine(StartCountdown());
+    }
+
+    IEnumerator StartCountdown()
+    {
+        for (int i = countdown; i > 0; i--)
+        {
+            countdownText.text = countdownMessage.Replace(countdownNumberReplacement, i.ToString("00"));
+            yield return new WaitForSeconds(1f);
+        }
+
+        StartCoroutine(MatchTime());
+    }
+
+    IEnumerator MatchTime()
+    {
+        int totalSeconds = matchTimeMinutes * 60 + matchTimeSeconds;
+        for (int i = totalSeconds; i > 0; i--)
+        {
+            int currentMinutes = i / 60;
+            int currentSeconds = (int)(((i / 60f) - currentMinutes) * 60f);
+
+            matchTimeText.text = matchTimeMessage.Replace(matchTimeMinutesReplacement, currentMinutes.ToString("00")).Replace(matchTimeSecondsReplacement, currentSeconds.ToString("00"));
+            yield return new WaitForSeconds(1f);
+        }
+
+        matchTimeText.text = "";
+        countdownText.text = endMatchMessage;
+    }
+}
